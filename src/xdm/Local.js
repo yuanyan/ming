3abb0683367503ab1d809c12d59ff6b5ffd585fb
yuanyan <yuanyan.cao@gmail.com>
2011-12-30 00:49:15 +0800

    xdm.Local update

--- src/xdm/Local.js
+++ src/xdm/Local.js
@@ -1,81 +1,58 @@
 /**
  * Clsss: Local
+ * 
+ * Example:
+ *  Local.postMessage("top", "hello world", "http://example.com", "http://example.com/proxy.html");
+ *	function onmessage(event){
+ *		var origin = event.origin,
+ *			data = event.data;
+ *	}
+ *  Event.on(window, "message", onmessage)
  *
+ * See: 
  * https://developer.mozilla.org/en/DOM/window.postMessage
+ * 
  */
 
 module("xdm.Local", function(global) {
 
 	var doc = document, 
-		Event = module("event.Event");
+	    JSON = module("lang.JSON"),
+		Event = module("event.Event"),
+		proxy_html;
 
-	var reURI = /^((http.?:)\/\/([^:\/\s]+)(:\d+)*)/;
-	var xdm_proxy;
-
-	function getLocation(url){
-	
-		if (!url) {
-			throw new Error("url is undefined or empty");
-		}
-		
-		if (/^file/.test(url)) {
-			throw new Error("The file:// protocol is not supported");
-		}
-		
-		var m = url.toLowerCase().match(reURI);
-		var proto = m[2], domain = m[3], port = m[4] || "";
-		if ((proto == "http:" && port == ":80") || (proto == "https:" && port == ":443")) {
-			port = "";
-		}
-		return proto + "//" + domain + port;
-	}
-	
-	function getOrigin(event){
-        if (event.origin) {
-            // This is the HTML5 property
-            return getLocation(event.origin);
-        }
-        if (event.uri) {
-            // From earlier implementations 
-            return getLocation(event.uri);
-        }
-        if (event.domain) {
-            // This is the last option and will fail if the 
-            // origin is not using the same schema as we are
-            return location.protocol + "//" + event.domain;
-        }
-        throw "Unable to retrieve the origin of the event";
-    }
-	
-	function onMessage(event){
-        var origin = getOrigin(event),
-			data = event.data,
-			source = event.source;
-
-    }
-	
-	window['__removeNode'] = function(node){
+	// remove node self
+	global['__removeNode'] = function(node){
 		node.parentNode.removeChild(node);
 	}
 	
+	// target object|string
+	// data string
+	// targetOrigin string
+	// proxy string
+	function postMessage(target, data, targetOrigin, proxy){
 	
-	function postMessage(target, message, targetOrigin, origin, proxy){
-		if(window['postMessage']){
-			target = ("string" === typeof target? eval(target): target);
-			target.postMessage(message, targetOrigin);
+		if( global['postMessage'] ){
+			target = ( "string" === typeof target? eval(target): target ); // target: parent\top 
+			target.postMessage(data, targetOrigin);
 			
 		}else{
-
-			proxy = proxy || 'http://' + origin + '/proxy.html#';
-			
-			if(!xdm_proxy){
-				xdm_proxy = doc.createElement("div");
-				doc.body.appendChild(xdm_proxy);
+			proxy = proxy || ( targetOrigin + '/proxy.html' );
+			if(!proxy_html){
+				proxy_html = doc.createElement("div");
+				doc.body.appendChild(proxy_html);
 			}
 			
-			xdm_proxy.innerHTML = '<iframe name="' + encodeURIComponent(message) + '" src="' + proxy + '" onload="__removeNode(this)"></iframe>';
+			var message = {
+				"data": data,
+				"target" : target,
+				"targetOrigin":  targetOrigin,
+				"origin": location.origin,
+				"source": "proxy"
+			}
+			
+			proxy_html.innerHTML = '<iframe name="' + encodeURIComponent( JSON.stringify(message) ) + '" src="' + proxy + '" onload="__removeNode(this)"></iframe>';
 
-				
 		}
 	}
 
