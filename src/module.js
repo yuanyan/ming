009cd99e8a4a62e2523e8d77741cd28242864bc5
yuanyan <yuanyan.cao@gmail.com>
2013-01-09 02:18:39 +0800

    set module.js index module

---
+++ src/module.js
@@ -0,0 +1,35 @@
+!(function (factory) {
+    if (typeof define === 'function') {
+        define([
+            '$',
+            './class/class',
+            './cookie/cookie',
+            './cookie/remove',
+            './cors/img',
+            './cors/xdr',
+            './history/history',
+            './json/json',
+            './jsonpi/jsonpi',
+            './key/key',
+            './memoize/memoize',
+            './mousewheel/mousewheel',
+            './object/keys',
+            './postmessage/postmessage',
+            './route/route',
+            './storage/storage',
+            './string/color',
+            './string/format',
+            './string/trim',
+            './template/template',
+            './touch/touch',
+            './url/url',
+            './uuid/uuid',
+            './validate/validate',
+            './uuid/uuid'
+        ], factory);
+    } else {
+        factory($);
+    }
+})(function ($) {
+
+});
--- src/url/url.js
+++ src/url/url.js
@@ -1,3 +1,8 @@
+// Ref:
+// Uniform Resource Identifier (URI): Generic Syntax   http://tools.ietf.org/html/rfc3986
+// http://blog.stevenlevithan.com/archives/parseuri
+// http://nodejs.org/api/url.html
+
 !(function (factory) {
     if (typeof define === 'function') {
         define(['$'], factory);
@@ -7,7 +12,6 @@
 })(function ($) {
     'use strict';
     var pluginName = 'url';
-
 
     /*
      var parser = document.createElement('a');
@@ -20,27 +24,47 @@
      parser.hash;     // => "#hash"
      parser.host;     // => "example.com:3000"
      */
-    $[pluginName] = function (href){
+    $[pluginName] = function (href, parseQueryString){
         var type = $.type(href);
 
         if(type == 'string'){
             // work perfectly in all modern browsers (including IE6)
             // @see http://james.padolsey.com/javascript/parsing-urls-with-the-dom/
-            var parser = document.createElement('a');
-            parser.href = href;
-            return parser;
+            var a = document.createElement('a');
+            a.href = href;
+            return {
+                href: href,
+                protocol: a.protocol,
+                hostname: a.hostname,
+                port: a.port,
+                search: a.search,
+                hash: a.hash,
+                pathname: a.pathname,
+                path: a.pathname + a.search,
+                query: (function(){
 
-        }else if(type == 'object'){
-            // TODO
-            var parts = [href['protocol'],href['hostname'],href['port'],href['pathname'],href['search'],href['hash']];
-            parts.join('');
+                    var qs = a.search.replace(/^\?/,'');
+                    if(!parseQueryString){
+                        return qs;
+                    }else{
+                        var ret = {},
+                            seg = qs.split('&'),
+                            len = seg.length, i = 0, s;
+                        for (;i<len;i++) {
+                            if (!seg[i]) { continue; }
+                            s = seg[i].split('=');
+                            ret[s[0]] = s[1];
+                        }
+                        return ret;
+                    }
+
+                })()
+            };
+
+
 
         }
 
     };
-
-    // Uniform Resource Identifier (URI): Generic Syntax   http://tools.ietf.org/html/rfc3986
-    // http://blog.stevenlevithan.com/archives/parseuri
-    // http://nodejs.org/api/url.html
 
 })
