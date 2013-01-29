66d36d3082f454167703d0d31cd96264cb9be9e1
元彦 <yuanyan.cao@gmail.com>
2013-01-29 20:24:30 +0800

    runner reset

--- test/runner/webdriver.js
+++ test/runner/webdriver.js
@@ -1,143 +1,118 @@
-var webdriver = require('wd')
-    , assert = require('assert');
-
-var browser = webdriver.remote(
-    "ondemand.saucelabs.com"
-    , 80
-    , "modulejs"
-    , "01acc19e-ac09-4594-b2fa-ea69690df91e"
-);
-
-browser.on('status', function(info){
-    console.log('\x1b[36m%s\x1b[0m', info);
-});
-
-browser.on('command', function(meth, path){
-    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
-});
-
-
-var baseDesiredInfo = {
-    tags: ["2.0"]
-    , build: "master"
-    , passed: true
-    , name: "ModuleJS"
-};
+var Cloud = require('mocha-cloud');
+var cloud = new Cloud('ModuleJS', 'modulejs', '01acc19e-ac09-4594-b2fa-ea69690df91e');
 
 var allDesired = [
     {
         browserName:'internet explorer',
         version: '6',
         platform: 'Windows 2003'
-    },
-    {
-        browserName:'internet explorer',
-        version: '7',
-        platform: 'Windows 2003'
-    },
-    {
-        browserName:'internet explorer',
-        version: '8',
-        platform: 'Windows 2003'
-    },
-    {
-        browserName:'internet explorer',
-        version: '9',
-        platform: 'Windows 2008'
-    },
-    {
-        browserName:'internet explorer',
-        version: '10',
-        platform: 'Windows 2012'
-    },
-    {
-        browserName:'chrome',
-        platform: 'Windows 2003'
-    },
-    {
-        browserName:'chrome',
-        platform: 'Mac 10.8'
-    },
-    {
-        browserName:'chrome',
-        platform: 'Linux'
-    },
-    {
-        browserName:'android',
-        platform: 'Linux'
-    },
-    {
-        browserName:'ipad',
-        platform: 'Mac 10.8',
-        version: '5.1'
-    },
-    {
-        browserName:'ipad',
-        platform: 'Mac 10.8',
-        version: '6'
-    },
-    {
-        browserName:'iphone',
-        platform: 'Mac 10.8',
-        version: '5.1'
-    },
-    {
-        browserName:'iphone',
-        platform: 'Mac 10.8',
-        version: '6'
-    },
-    {
-        browserName:'firefox',
-        platform: 'Windows 2003'
-    },
-    {
-        browserName:'firefox',
-        platform: 'Windows 2008'
-    },
-    {
-        browserName: 'safari',
-        version: '5',
-        platform: 'Windows 2008'
-    },
-    {
-        browserName: 'safari',
-        version: '5',
-        platform: 'Mac 10.8'
-    },
-    {
-        browserName: 'opera',
-        version: '11',
-        platform: 'Windows 2003'
-    },
-    {
-        browserName: 'opera',
-        version: '12',
-        platform: 'Windows 2008'
     }
+//    ,{
+//        browserName:'internet explorer',
+//        version: '7',
+//        platform: 'Windows 2003'
+//    },
+//    {
+//        browserName:'internet explorer',
+//        version: '8',
+//        platform: 'Windows 2003'
+//    },
+//    {
+//        browserName:'internet explorer',
+//        version: '9',
+//        platform: 'Windows 2008'
+//    },
+//    {
+//        browserName:'internet explorer',
+//        version: '10',
+//        platform: 'Windows 2012'
+//    },
+//    {
+//        browserName:'chrome',
+//        platform: 'Windows 2003'
+//    },
+//    {
+//        browserName:'chrome',
+//        platform: 'Mac 10.8'
+//    },
+//    {
+//        browserName:'chrome',
+//        platform: 'Linux'
+//    },
+//    {
+//        browserName:'android',
+//        platform: 'Linux'
+//    },
+//    {
+//        browserName:'ipad',
+//        platform: 'Mac 10.8',
+//        version: '5.1'
+//    },
+//    {
+//        browserName:'ipad',
+//        platform: 'Mac 10.8',
+//        version: '6'
+//    },
+//    {
+//        browserName:'iphone',
+//        platform: 'Mac 10.8',
+//        version: '5.1'
+//    },
+//    {
+//        browserName:'iphone',
+//        platform: 'Mac 10.8',
+//        version: '6'
+//    },
+//    {
+//        browserName:'firefox',
+//        platform: 'Windows 2003'
+//    },
+//    {
+//        browserName:'firefox',
+//        platform: 'Windows 2008'
+//    },
+//    {
+//        browserName: 'safari',
+//        version: '5',
+//        platform: 'Windows 2008'
+//    },
+//    {
+//        browserName: 'safari',
+//        version: '5',
+//        platform: 'Mac 10.8'
+//    },
+//    {
+//        browserName: 'opera',
+//        version: '11',
+//        platform: 'Windows 2003'
+//    },
+//    {
+//        browserName: 'opera',
+//        version: '12',
+//        platform: 'Windows 2008'
+//    }
 ];
 
-var extend = function(destination, source) {
+allDesired.forEach(function(desired){
+    cloud.browser(desired.browserName, desired.version || '', desired.platform);
+});
 
-    if (source) {
-        for (var prop in source) {
-            destination[prop] = source[prop];
-        }
-    }
+cloud.tags =  ["2.0.0-dev"];
+cloud.name = "ModuleJS";
 
-    return destination;
-};
+cloud.url('http://modulejs.github.com/modulejs/test/index.html');
 
-allDesired.forEach(function(val){
+cloud.on('init', function(browser){
+    console.log('  init : %s %s', browser.browserName, browser.version);
+});
 
-    var desired = extend(val, baseDesiredInfo);
+cloud.on('start', function(browser){
+    console.log('  start : %s %s', browser.browserName, browser.version);
+});
 
-    browser.init(desired, function() {
+cloud.on('end', function(browser, res){
+    console.log('  end : %s %s : %d failures', browser.browserName, browser.version, res.failures);
+});
 
-        browser.get("http://modulejs.github.com/modulejs/test/index.html", function() {
-
-            browser.quit()
-        })
-    })
-})
-
+cloud.start();
 
