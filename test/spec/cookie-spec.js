23c02cd7fb3b0679536de2daa88385ba5e38cf89
yuanyan <yuanyan.cao@gmail.com>
2013-02-25 22:56:32 +0800

    fix spec error

--- test/spec/cookie-spec.js
+++ test/spec/cookie-spec.js
@@ -1,6 +1,6 @@
 var expect = require('../lib/expect');
 require('../../lib/jquery/jquery');
-var $ = require('../../dist/module');
+require('../../dist/module');
 
 describe("cookie module", function() {
 
