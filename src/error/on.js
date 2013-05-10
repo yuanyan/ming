4c1061d11420ab45b1e6f72eec43f305e3e1e7cd
元彦 <yuanyan.cao@gmail.com>
2013-05-10 12:41:02 +0800

    jQuery.fn.on proxy

---
+++ src/error/on.js
@@ -0,0 +1,84 @@
+!(function (factory) {
+    if (typeof define === 'function') {
+        define(['$'], factory);
+    } else {
+        factory($);
+    }
+})(function ($) {
+    'use strict';
+
+    function returnFalse() {
+        return false;
+    }
+
+    jQuery.fn.extend({
+
+        on: function( types, selector, data, fn, /*INTERNAL*/ one ) {
+
+            var origFn, type;
+
+            // Types can be a map of types/handlers
+            if ( typeof types === "object" ) {
+                // ( types-Object, selector, data )
+                if ( typeof selector !== "string" ) {
+                    // ( types-Object, data )
+                    data = data || selector;
+                    selector = undefined;
+                }
+                for ( type in types ) {
+                    this.on( type, selector, data, types[ type ], one );
+                }
+                return this;
+            }
+
+            if ( data == null && fn == null ) {
+                // ( types, fn )
+                fn = selector;
+                data = selector = undefined;
+            } else if ( fn == null ) {
+                if ( typeof selector === "string" ) {
+                    // ( types, selector, fn )
+                    fn = data;
+                    data = undefined;
+                } else {
+                    // ( types, data, fn )
+                    fn = data;
+                    data = selector;
+                    selector = undefined;
+                }
+            }
+            if ( fn === false ) {
+                fn = returnFalse;
+            } else if ( !fn ) {
+                return this;
+            }
+
+            // event callback sandbox
+            origFn = fn;
+            fn = function(){
+                try{
+                    return origFn.apply( this, arguments );
+                } catch ( e ){
+                    // throw custom error, catch it on window.onerror
+                    throw Error(['jQueryEventError', types, selector, e.stack].join(','))
+                }
+            };
+
+            if ( one === 1 ) {
+                origFn = fn;
+                fn = function( event ) {
+                    // Can use an empty set, since event contains the info
+                    jQuery().off( event );
+                    return origFn.apply( this, arguments );
+                };
+                // Use same guid so caller can remove using origFn
+                fn.guid = origFn.guid || ( origFn.guid = jQuery.guid++ );
+            }
+
+            return this.each( function() {
+                jQuery.event.add( this, types, fn, data, selector );
+            });
+        }
+
+    });
+})
